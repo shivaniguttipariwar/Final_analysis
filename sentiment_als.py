ec4acac8b91dd06325b7865451dd9f9dfb970b06
Shivani Guttipratiwar <116349375+shivaniguttipariwar@users.noreply.github.com>
2023-03-08 23:25:27 +0530

    Update sentiment_als.py

--- sentiment_als.py
+++ sentiment_als.py
@@ -3,10 +3,12 @@ import numpy as np
 import pickle
 import streamlit as st
 import nltk
-nltk.download ("stopwords")
+
+nltk.download("stopwords")
 nltk.download("punkt")
 nltk.download('wordnet')
-nltk.download('omw-1.4')
+nltk.download('omw')
+
 import re
 import string
 from textblob import Word
@@ -14,25 +16,21 @@ from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
 if __name__ == '__main__':
     st.title('Financial Sentiment Analysis :bar_chart:')
     st.write('A simple sentiment analysis classification app')
     st.subheader('Give your input below')
-    sentence = st.text_area('Enter your text here',height=200)
-    predict_btt = st.button('predict')
+    sentence = st.text_area('Enter your text here', height=200)
+    predict_btt = st.button('Predict')
     loaded_model = pickle.load(open('sentiment_analysis.p', 'rb')) 
-if predict_btt:
-    disp=" "
-	
-a = loaded_model.predict([sentence])[0]
-if a == 1:
-    disp = "positive sentiment"
-elif a == 0:
-    disp = "negative sentiment"
-else:
-    disp = "neutral sentiment"
-st.write('The sentiment of the given text is: ', disp)
 
-
-        
+    if predict_btt:
+        disp = " "
+        a = loaded_model.predict([sentence])[0]
+        if a == 1:
+            disp = "positive sentiment"
+        elif a == 0:
+            disp = "negative sentiment"
+        else:
+            disp = "neutral sentiment"
+        st.write('The sentiment of the given text is: ', disp)
